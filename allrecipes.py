b873830614f62e100ea912eaa5804fee9a6cd2d5
Mike Lewis <mikelew88@gmail.com>
2016-03-19 19:14:02 -0600

    Now I need to store images in mongodb

--- allrecipes.py
+++ allrecipes.py
@@ -10,8 +10,13 @@ from pymongo import MongoClient
 import gridfs
 import mimetypes
 
+import pdb
 
 def Pull_Recipe_Links(start_url='http://allrecipes.com/recipes/', limit=99999):
+    """define opener"""
+    class MyOpener(urllib.FancyURLopener):
+        version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+    myopener = MyOpener()
 
     Page_Error = False
     current_url = start_url
@@ -19,46 +24,49 @@ def Pull_Recipe_Links(start_url='http://allrecipes.com/recipes/', limit=99999):
     recipe_dict = defaultdict(str)
 
     while not Page_Error and i<limit:
-        print(i)
-        try:
 
-            page = myopener.open(current_url)
-            soup = BeautifulSoup(page)
+        # try:
 
-            Link_Soup = soup.findAll('a')
+        page = myopener.open(current_url)
+        soup = BeautifulSoup(page)
 
-            for a in Link_Soup:
-                link = str(a.get('href')).strip()
+        Link_Soup = soup.findAll('a')
 
-                #Parse url string to locate recipe name and number
-                end_recipe_number = link[8:].find('/')+8
-                recipe_number = link[8:end_recipe_number]
-                recipe_name = link[end_recipe_number+1:link[end_recipe_number+1:].find('/')+end_recipe_number+1]
+        for a in Link_Soup:
+            link = str(a.get('href')).strip()
 
-                #Store recipe information in a default dictionary with the recipe number as the key (in case there are duplicate recipe names)
-                if link[:8]=='/recipe/':
+            #Parse url string to locate recipe name and number
+            end_recipe_number = link[8:].find('/')+8
+            recipe_number = link[8:end_recipe_number]
+            recipe_name = link[end_recipe_number+1:link[end_recipe_number+1:].find('/')+end_recipe_number+1]
 
+            #Store recipe information in a default dictionary with the recipe number as the key (in case there are duplicate recipe names)
 
-                    ingreds, image_page_link = scrape_ingredients(link)
+            # pdb.set_trace()
+#
+            print link
+            if link[:8]=='/recipe/':
 
-                    image_links = scrape_photos(image_page_link)
+                ingreds, image_page_link = scrape_ingredients(link)
 
-                    recipe_dict[recipe_number] = [recipe_name, link, ingreds, image_links]
+                image_links = scrape_photos(image_page_link)
 
-            i += 1
-            current_url = "http://allrecipes.com/recipes/?page=" + str(i)
+                recipe_dict[recipe_number] = [recipe_name, link, ingreds, image_links]
 
-        except Exception:
-            Page_Error = True
+        i += 1
+        current_url = "http://allrecipes.com/recipes/?page=" + str(i)
+
+        # except Exception:
+        #     Page_Error = True
 
     return recipe_dict
 
 def scrape_ingredients(link):
 
-    link = val[0]
     url = "http://allrecipes.com"+link
     data = urllib2.urlopen(url).read()
     soup = BeautifulSoup(data)
+    # pdb.set_trace()
 
     image_page_link = soup.findAll('a', {'class':'icon-photoPage'})[0].get('href')
 
@@ -71,7 +79,7 @@ def scrape_ingredients(link):
     return ingreds, image_page_link
 
 def scrape_photos(image_page_link, num_photos = 25):
-    url = "http://allrecipes.com"+link
+    url = "http://allrecipes.com"+image_page_link
     data = urllib2.urlopen(url).read()
     soup = BeautifulSoup(data)
 
@@ -91,14 +99,9 @@ def scrape_photos(image_page_link, num_photos = 25):
 def store_data(recipe_dict):
     mime_type = mimetypes.guess_type(image_url)[0]
 
-    a = fs.put(image_url, contentType=mime_type, filename='temp.jpg')
+    a = fs.put(image_url, contentType=mime_type, filename=str('temp_imgname'+'.jpg'))
 
 def run_scrapper():
-    """define opener"""
-    class MyOpener(urllib.FancyURLopener):
-        version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-    myopener = MyOpener()
-
     #run mongod first!
 
     db_client = MongoClient()
