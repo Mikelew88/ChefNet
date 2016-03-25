e3b04b8ca361696620bb10d14b4ce07e0eb063bd
Mike Lewis <mikelew88@gmail.com>
2016-03-24 18:21:38 -0600

    Making parallel

--- allrecipes.py
+++ allrecipes.py
@@ -9,49 +9,46 @@ import re
 from pymongo import MongoClient
 import gridfs
 import mimetypes
+import multiprocessing
+import threading
 
 import pdb
 
-def Pull_Recipe_Links(limit=99999):
+def Pull_Recipe_Links(page):
     """define opener"""
     class MyOpener(urllib.FancyURLopener):
         version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
     myopener = MyOpener()
 
-    current_url = 'http://allrecipes.com/recipes/'
-    i = 0
+    url = "http://allrecipes.com/recipes/?page=" + str(i)
     recipe_dict = defaultdict(str)
 
-    while i<limit:
+    page = myopener.open(url)
+    soup = BeautifulSoup(page)
 
-        page = myopener.open(current_url)
-        soup = BeautifulSoup(page)
+    Link_Soup = soup.findAll('a')
 
-        Link_Soup = soup.findAll('a')
+    for a in Link_Soup:
+        link = str(a.get('href')).strip()
 
-        for a in Link_Soup:
-            link = str(a.get('href')).strip()
+        #Parse url string to locate recipe name and number
+        end_recipe_number = link[8:].find('/')+8
+        recipe_number = link[8:end_recipe_number]
+        recipe_name = link[end_recipe_number+1:link[end_recipe_number+1:].find('/')+end_recipe_number+1]
 
-            #Parse url string to locate recipe name and number
-            end_recipe_number = link[8:].find('/')+8
-            recipe_number = link[8:end_recipe_number]
-            recipe_name = link[end_recipe_number+1:link[end_recipe_number+1:].find('/')+end_recipe_number+1]
+        #Store recipe information in a default dictionary with the recipe number as the key (in case there are duplicate recipe names)
 
-            #Store recipe information in a default dictionary with the recipe number as the key (in case there are duplicate recipe names)
+        print link
+        if link[:8]=='/recipe/':
 
-            print link
-            if link[:8]=='/recipe/':
+            ingreds, image_page_link = scrape_ingredients(link)
 
-                ingreds, image_page_link = scrape_ingredients(link)
+            image_links = scrape_photos(image_page_link)
 
-                image_links = scrape_photos(image_page_link)
-
-                recipe_dict[recipe_number] = [recipe_name, link, ingreds, image_links]
-
-        i += 1
-        current_url = "http://allrecipes.com/recipes/?page=" + str(i)
+            recipe_dict[recipe_number] = [recipe_name, link, ingreds, image_links]
 
     return recipe_dict
+
 
 def scrape_ingredients(link):
 
@@ -87,6 +84,13 @@ def scrape_photos(image_page_link, num_photos = 25):
             i+=1
 
     return image_links
+    # For threading
+    # jobs=[]
+    # for business in ids['businesses']:
+    #     business_path = BUSINESS_PATH + business['id']
+    #     t = threading.Thread(target=request_business, args=(business_path,))
+    #     jobs.append(t)
+    #     t.start()
 
 def store_data(recipe_dict):
 
@@ -107,6 +111,11 @@ def store_data(recipe_dict):
             mime_type = mimetypes.guess_type(image_url)[0]
 
             a = fs.put(image_url, contentType=mime_type, filename=str(key+'_'+str(i)+'.jpg'))
+
+def parallel():
+    page_range = range(1,100)
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    outputs = pool.map(Pull_Recipe_Links, page_range)
 
 if __name__ == '__main__':
     recipe_dict = Pull_Recipe_Links(limit = 9999)
