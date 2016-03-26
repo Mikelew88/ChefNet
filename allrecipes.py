b77e3e660061d3ea6e55293bd998977e2f5dff67
Mike Lewis <mikelew88@gmail.com>
2016-03-25 18:08:39 -0600

    Scraping more content

--- allrecipes.py
+++ allrecipes.py
@@ -20,7 +20,6 @@ def Pull_Recipe_Links(page):
     myopener = MyOpener()
 
     url = "http://allrecipes.com/recipes/?page=" + str(i)
-    recipe_dict = defaultdict(str)
 
     page = myopener.open(url)
     soup = BeautifulSoup(page)
@@ -41,12 +40,12 @@ def scrape_item(link):
 
     if link[:8]=='/recipe/':
 
-        scrape_ingredients(recipe_id, link)
+        scrape_sub_page(recipe_id, link)
 
     return recipe_dict
 
 
-def scrape_ingredients(recipe_id, link):
+def scrape_sub_page(recipe_id, link):
 
     url = "http://allrecipes.com"+link
     data = urllib2.urlopen(url).read()
@@ -56,13 +55,35 @@ def scrape_ingredients(recipe_id, link):
     image_page_link = soup.findAll('a', {'class':'icon-photoPage'})[0].get('href')
     scrape_photos(image_page_link, )
 
-    ingreds = []
+    ingred_list = []
+
+    stars = soup.find('div', {'class':'rating-stars'}).get('data-ratingstars')
+
+    submitter_name = soup.find('span', {'class':'submitter__name'}).text
+
+    submitter_desc = soup.find('div', {'class':'submitter__description'}).text
+
+    item_name = soup.find('h1', {'class':'recipe-summary__h1'}).text
+
     for s in soup.findAll('li', {'class': 'checkList__line'}):
         ingred = s.text.strip()
         if not ingred.startswith('Add') and not ingred.startswith('ADVERTISEMEN'):
-            ingreds.append(ingred[:s.text.strip().find('\n')])
+            ingred_list.append(ingred[:s.text.strip().find('\n')])
 
-    ingred_db.insert({recipe_id:ingreds})
+    dircetions = soup.find('div', {'class':'directions--section'})
+
+    prep_time = dircetions.find('time', {'itemprop':'prepTime'}).get('datetime')
+    cook_time = dircetions.find('time', {'itemprop':'cookTime'}).get('datetime')
+    total_time = = dircetions.find('time', {'itemprop':'totalTime'}).get('datetime')
+
+    dircetions = dircetions.findAll('span', {'class':'recipe-directions__list--item'})
+    direction_list = [d.text for d in directions]
+
+    cat_soup = soup.find('div', {'class': ['tab-pane', 'ng-scope', 'ng-isolate-scope'],'title':'Categories'})
+    cats = cat_soup.findAll('h3', {'class':'grid-col__h3'})
+    cat_text = [cat.text for cat in cats]
+
+    ingred_db.insert({'id':recipe_id, 'item_name': item_name, 'ingred_list':ingred_list, 'direction_list':direction_list, 'stars': stars, 'submitter_name':submitter_name, 'submitter_desc': submitter_desc, 'prep_time':prep_time, 'cook_time':cook_time, 'total_time':total_time, 'cat_text':cat_text})
     pass
 
 def scrape_photos(image_page_link, recipe_id, num_photos = 25):
