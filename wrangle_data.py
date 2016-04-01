919412607aa9c879c948038efad5bd051697141c
Mike Lewis <mikelew88@gmail.com>
2016-04-01 15:53:04 -0600

    testing preliminary model

---
+++ wrangle_data.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient import pandas as pd
+
+if __name__ == '__main__':
+    #Store results in mongo
+    db_client = MongoClient()
+    db = db_client['allrecipes']
+    recipe_db = db['recipe_data']
+
+    df = pd.DataFrame(list(recipe_db.find()))
+
+    ingred_list = df[['id','ingred_list']]
