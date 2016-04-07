396edb96b099e50523906c09d53f29641402d535
Mike Lewis <mikelew88@gmail.com>
2016-04-07 10:22:12 -0600

    except IOError

--- preprocess_data.py
+++ preprocess_data.py
@@ -108,19 +108,18 @@ def vectorize_imgs(img_paths):
     img_list = []
     bad_images = []
 
-
-    for img, file_loc in zip(img_gen, img_gen.files):
-        if len(img.shape) != 3:
-            print 'Blank Image: {}'.format(file_loc)
-            os.rename(file_loc, "data/Bad_Images/"+file_loc.split('/')[-1])
-            bad_images.append(file_loc)
-        else:
-            try:
-                img_list.append(img)
-            except:
-                print 'Corrupted File: {}'.format(file_loc)
+    try:
+        for img, file_loc in zip(img_gen, img_gen.files):
+            if len(img.shape) != 3:
+                print 'Blank Image: {}'.format(file_loc)
                 os.rename(file_loc, "data/Bad_Images/"+file_loc.split('/')[-1])
                 bad_images.append(file_loc)
+            else:
+                img_list.append(img)
+    except IOError:
+        print 'Corrupted File: {}'.format(file_loc)
+        os.rename(file_loc, "data/Bad_Images/"+file_loc.split('/')[-1])
+        bad_images.append(file_loc)
 
     return np.array(img_list), bad_images
 
