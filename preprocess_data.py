ce0bb998763edf7739dc3c5b947a6ad6a1c3d6f1
Mike Lewis <mikelew88@gmail.com>
2016-04-07 10:11:12 -0600

    try except bad image files

--- preprocess_data.py
+++ preprocess_data.py
@@ -112,10 +112,16 @@ def vectorize_imgs(img_paths):
 
     for img, file_loc in zip(img_gen, img_gen.files):
         if len(img.shape) != 3:
-            print 'Issue with image: {}'.format(file_loc)
+            print 'Blank Image: {}'.format(file_loc)
+            os.rename(file_loc, "data/Bad_Images/"+file_loc.split('/')[-1])
             bad_images.append(file_loc)
         else:
-            img_list.append(img)
+            try:
+                img_list.append(img)
+            except:
+                print 'Corrupted File: {}'.format(file_loc)
+                os.rename(file_loc, "data/Bad_Images/"+file_loc.split('/')[-1])
+                bad_images.append(file_loc)
 
     return np.array(img_list), bad_images
 
