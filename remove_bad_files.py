50335ccf22cca1ce7eae38b8fc23e75316684f5e
Mike Lewis <mikelew88@gmail.com>
2016-04-07 10:35:20 -0600

    added jpg scrubber

--- remove_bad_files.py
+++ remove_bad_files.py
@@ -13,11 +13,11 @@ def remove_bad_jpgs(dir_path='/data/Recipe_Images'):
     img_dir = os.listdir(dir_path)
 
     for jpg in img_dir:
-        if os.path.getsize(dir_path+jpg) < .001:
-            os.path.getsize('C:\\Python27\\Lib\\genericpath.py')
-            print 'Corrupted File: {}'.format(file_loc)
-            os.rename(file_loc, "data/Bad_Images/"+file_loc.split('/')[-1])
-            bad_images.append(file_loc)
+        jpg_path = dir_path+jpg
+        if os.path.getsize(jpg_path) < .001:
+            os.path.getsize(jpg_path)
+            print 'Corrupted File: {}'.format(jpg_path)
+            os.rename(jpg_path, "/data/Bad_Images/"+jpg)
 
 if __name__ == '__main__':
     remove_bad_jpgs()
